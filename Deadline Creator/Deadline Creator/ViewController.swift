8133c7fe30d9e2454540365d0734d65a58de6fd6
Choi Sehyun <schoiaj@connect.ust.hk>
2019-04-13 19:42:12 +0800

    UI

---
+++ Deadline Creator/Deadline Creator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deadline Creator
+//
+//  Created by Sam Choi on 13/4/2019.
+//  Copyright © 2019 CS17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
